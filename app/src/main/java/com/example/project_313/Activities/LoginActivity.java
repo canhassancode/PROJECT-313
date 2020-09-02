531a93527b24b8405ef46da194ea3335d08852d9
Hassan <hassanali25@hotmail.co.uk>
2020-09-02 17:19:40 +0100

    Register done and login started

--- app/src/main/java/com/example/project_313/Activities/LoginActivity.java
+++ app/src/main/java/com/example/project_313/Activities/LoginActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -16,14 +18,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText userMail,userPassword;
     private Button btnLogin;
+    private Button btnRegister;
     private ProgressBar loginProgress;
     private FirebaseAuth mAuth;
     private Intent HomeActivity;
+    private ImageView loginPhoto;
 
 
     @Override
@@ -37,6 +42,19 @@ public class LoginActivity extends AppCompatActivity {
         loginProgress = findViewById(R.id.progressBar2);
         mAuth = FirebaseAuth.getInstance();
         HomeActivity = new Intent(this,com.example.project_313.Activities.HomeActivity.class);
+        btnRegister = findViewById(R.id.reg_btn);
+
+//        loginPhoto = findViewById(R.id.logUserPic);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+
+            }
+        });
 
         loginProgress.setVisibility(View.INVISIBLE);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +70,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (mail.isEmpty() || password.isEmpty()) {
                     showMessage("Please verify all Fields");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
                 }
                 else {
                     signin(mail,password);
@@ -72,6 +92,11 @@ public class LoginActivity extends AppCompatActivity {
                     btnLogin.setVisibility(View.VISIBLE);
                     updateUI();
                 }
+                else {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
 
             }
         });
@@ -79,9 +104,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
-
-        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
-        startActivity(homeActivity);
+        startActivity(HomeActivity);
         finish();
 
     }
@@ -90,5 +113,17 @@ public class LoginActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            // user already connected
+            updateUI();
+
+        }
     }
 }
