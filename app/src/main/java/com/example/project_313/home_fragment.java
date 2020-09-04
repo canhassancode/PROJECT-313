d62c09623252b121ca9fd6fc9c445839caa3a82c
Hassan <hassanali25@hotmail.co.uk>
2020-09-04 21:55:14 +0100

    Loads data, not in fragment_home_fragment.xml

--- app/src/main/java/com/example/project_313/home_fragment.java
+++ app/src/main/java/com/example/project_313/home_fragment.java
@@ -2,63 +2,97 @@ package com.example.project_313;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.project_313.Activities.HomeActivity;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+
 public class home_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView mFirestoreList;
+    private FirebaseFirestore firebaseFirestore;
+    private FirestoreRecyclerAdapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View view;
 
-    public home_fragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static home_fragment newInstance(String param1, String param2) {
-        home_fragment fragment = new home_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        view =  inflater.inflate(R.layout.fragment_home_fragment, container, false);
+
+        mFirestoreList = view.findViewById(R.id.noha_list);
+        setUpRecyclerView();
+
+        // Database list
+
+        return view;
+    }
+
+    public void setUpRecyclerView() {
+        //Query INVESTIGATE .OPTIONS AFTER
+        this.firebaseFirestore = FirebaseFirestore.getInstance();
+        Query query = firebaseFirestore.collection("nohay");
+
+        //RecyclerOptions
+        FirestoreRecyclerOptions<NohayModel> options = new FirestoreRecyclerOptions.Builder<NohayModel>()
+                .setQuery(query, NohayModel.class)
+                .build();
+
+
+        this.adapter = new FirestoreRecyclerAdapter<NohayModel, NohayViewHolder>(options) {
+            @NonNull
+            @Override
+            public NohayViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                // Noha layout fullscreen options
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.noha_fullscreen, parent, false);
+
+                // return to noha class
+
+                return new home_fragment.NohayViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull NohayViewHolder holder, int position, @NonNull NohayModel model) {
+                holder.txtTitle.setText(model.getTitle());
+                holder.txtNoha.setText(model.getNoha());
+            }
+        };
+//        mFirestoreList.setHasFixedSize(true);
+        mFirestoreList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mFirestoreList.setAdapter(adapter);
+
+    }
+
+    public class NohayViewHolder extends RecyclerView.ViewHolder  {
+        // TextView from layout
+        private TextView txtTitle;
+        private TextView txtNoha;
+
+
+        public NohayViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtNoha = itemView.findViewById(R.id.txtNoha);
+        }
     }
 }
